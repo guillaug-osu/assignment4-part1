8226bbdde5d1766352735a4fcbacb920a91c843a
guillaug-osu <guillaug@onid.oregonstate.edu>
2015-05-01 22:17:02 +0000

    Added if statement

--- src/content1.php
+++ src/content1.php
@@ -1,8 +1,14 @@
 <?php
+//require_once( 'php/auth.php');
 
-$username = $_POST["username"];
+if (isset($_POST["username"]) && !empty($_POST["username"]))
+{
+    echo $_POST["username"];
+}
+else
+{
+    echo "A username must be entered. Click <a href='login.php'>here</a> to return to the login screen.";
+}
 
-
-echo $username;
 
 //echo json_encode();
