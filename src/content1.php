332e403dc4fece8351717c2b19f23433d727a825
guillaug-osu <guillaug@onid.oregonstate.edu>
2015-05-01 23:07:17 +0000

    Changed if statement

--- src/content1.php
+++ src/content1.php
@@ -1,14 +1,17 @@
 <?php
-//require_once( 'php/auth.php');
+
 
 if (isset($_POST["username"]) && !empty($_POST["username"]))
 {
-    echo $_POST["username"];
+
+    session_start();
+	$_SESSION['username'] = $_POST["username"];
+	$_SESSION['count'] = 0;
+    session_write_close();
+    header("location: content2.php");
+    exit();
 }
 else
 {
     echo "A username must be entered. Click <a href='login.php'>here</a> to return to the login screen.";
 }
-
-
-//echo json_encode();
