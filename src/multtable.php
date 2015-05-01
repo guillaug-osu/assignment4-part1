1724cf172e555224a84909a5d4f077f054fe4b75
guillaug-osu <guillaug@onid.oregonstate.edu>
2015-05-01 14:36:10 +0000

    added source folder

---
+++ src/multtable.php
@@ -0,0 +1,13 @@
+<?php
+
+$min_multiplicand = $_GET["min-multiplicand"];
+$max_multiplicand = $_GET["max-multiplicand"];
+$min_multiplier = $_GET["min-multiplier"];
+$max_multiplier = $_GET["max-multiplier"];
+
+echo $min_multiplicand;
+echo $max_multiplicand;
+echo $min_multiplier;
+echo $max_multiplier;
+
+//echo json_encode();
