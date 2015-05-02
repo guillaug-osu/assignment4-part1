b83a052fc52075f6bfb1dd7309a88e1569c9b490
guillaug-osu <guillaug@onid.oregonstate.edu>
2015-05-02 18:18:07 +0000

    Fixed sequence of if statements and added styling for tables

--- src/multtable.php
+++ src/multtable.php
@@ -5,9 +5,41 @@ $max_multiplicand = $_GET["max-multiplicand"];
 $min_multiplier = $_GET["min-multiplier"];
 $max_multiplier = $_GET["max-multiplier"];
 
-echo $min_multiplicand;
-echo $max_multiplicand;
-echo $min_multiplier;
-echo $max_multiplier;
+$values = array($min_multiplicand, $max_multiplicand, $min_multiplier, $max_multiplier);
+foreach ($values as $value) {
+	if(!intval($value))
+	{
+		echo $value, " value must be an integer\n";
+	}
+}
 
-//echo json_encode();
+
+if($max_multiplicand < $min_multiplicand)
+{
+	echo "Minimum, ", $min_multiplicand," larger than maximum.";
+}
+else
+{
+
+	echo "<table style='border: 1px solid black; border-collapse: collapse;' width='600'>";
+	
+		echo "<tr><th style='border: 1px solid black;'></th>";
+	for($j = $min_multiplier; $j <= $max_multiplier; $j++ )
+	{
+		echo "<th style='border: 1px solid black;'>", $j, "</th>";
+	}
+		echo "</tr>";
+	
+	for($i = $min_multiplicand; $i <= $max_multiplicand; $i++)
+	{
+		echo "<tr><td style='border: 1px solid black;'>", $i,"</td>";
+		for($j = $min_multiplier; $j <= $max_multiplier; $j++)
+		{
+			echo "<td style='border: 1px solid black;'>",$i * $j, "</td>";
+		}
+		echo "</tr>";
+	}
+	
+	echo "</table>";
+}
+
