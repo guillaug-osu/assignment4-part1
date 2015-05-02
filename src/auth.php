6ed8caf6f7b4f2b7d28745d845e2c29c0f57f6ff
guillaug-osu <guillaug@onid.oregonstate.edu>
2015-05-02 18:14:46 +0000

    changed if statement

--- src/auth.php
+++ src/auth.php
@@ -1,7 +1,7 @@
 <?php
 	//Start session
 	session_start();
-	//Check whether the session variable SESS_MEMBER_ID is present or not
+
 	if(!isset($_SESSION['username']) || (trim($_SESSION['username']) == ''))
 	{
 		header("location: login.php");
